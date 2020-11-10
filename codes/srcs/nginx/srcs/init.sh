6fe49d0ef11cec10260431d185f9fe19f6e876bc
paikwiki <paikwiki@gmail.com>
2020-11-10 18:35:11 +0900

    Add openssh-server on nginx Dockerfile

--- codes/srcs/nginx/srcs/init.sh
+++ codes/srcs/nginx/srcs/init.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+ssh-keygen -A
+adduser --disabled-password nginx_user
+echo "nginx_user:password" | chpasswd
+/usr/sbin/sshd
+
+mv /tmp/localhost.conf /etc/nginx/conf.d/
+rm /etc/nginx/conf.d/default.conf
+
 # mkdir -p /var/run/nginx
 #   nginx pid를 생성하기 위해 필요한 명령
 #   실행하지 않을 경우 "[emerg] open() "/run/nginx/nginx.pid" failed \
 #   (2: No such file or directory)" 오류 발생
 mkdir -p /var/run/nginx
-mv /tmp/localhost.conf /etc/nginx/conf.d/
-rm /etc/nginx/conf.d/default.conf
-
 nginx -g "daemon off;"

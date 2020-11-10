ae1e3dfdf9340474c5dc56524ac9f0ee616e1816
paikwiki <paikwiki@gmail.com>
2020-11-10 18:24:34 +0900

    Add a command which is omitted by mistake

--- codes/srcs/nginx/srcs/init.sh
+++ codes/srcs/nginx/srcs/init.sh
@@ -4,6 +4,7 @@
 #   nginx pid를 생성하기 위해 필요한 명령
 #   실행하지 않을 경우 "[emerg] open() "/run/nginx/nginx.pid" failed \
 #   (2: No such file or directory)" 오류 발생
+mkdir -p /var/run/nginx
 mv /tmp/localhost.conf /etc/nginx/conf.d/
 rm /etc/nginx/conf.d/default.conf
 

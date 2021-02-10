b40a572471e56a92dcffe36a7130a2a79b597273
paikwiki <paikwiki@gmail.com>
2021-02-10 19:54:29 +0900

    Create pods(mysql, phpmyadmin, wordpress(WIP))

--- codes/setup.sh
+++ codes/setup.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-# kubectl proxy &
+minikube start --driver=virtualbox
+
+eval $(minikube docker-env)
 
 docker build -t service-mysql ./srcs/mysql/
-# docker build -t service-phpmyadmin ./srcs/phpmyadmin/
+docker build -t service-phpmyadmin ./srcs/phpmyadmin/
+docker build -t service-wordpress ./srcs/wordpress/
 
 kubectl apply -f ./srcs/mysql/mysql.yaml
-# kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
+kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
+kubectl apply -f ./srcs/wordpress/wordpress.yaml
 
 # kubectl delete -f ./srcs/phpmyadmin/phpmyadmin.yaml;kubectl delete -f ./srcs/mysql/mysql.yaml
+# kubectl port-forward phpmyadmin-7cf4c5c785-mz25w 5000:80 -n default

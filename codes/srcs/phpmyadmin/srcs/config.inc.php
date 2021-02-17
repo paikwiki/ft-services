27fd401e52a7e2eccef403603aa1cc65bd1d67c9
paikwiki <paikwiki@gmail.com>
2021-02-17 20:45:45 +0900

    Add grafana

--- codes/srcs/phpmyadmin/srcs/config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-    declare(strict_types=1);
-    $i = 0;
-    $i++;
-    $cfg['Servers'][$i]['host'] = 'mysql';
-    $cfg['blowfish_secret'] = 'my_blowfish_secret_text';
-    $cfg['PmaAbsoluteUri'] = './';
--- codes/srcs/phpmyadmin/srcs/init.sh
+++ codes/srcs/phpmyadmin/srcs/init.sh
@@ -3,7 +3,6 @@
 # https://files.phpmyadmin.net/phpMyAdmin/5.0.4/phpMyAdmin-5.0.4-all-languages.tar.gz
 tar -xf /tmp/phpMyAdmin-5.0.4-all-languages.tar.gz -C /tmp/
 mv -f /tmp/phpMyAdmin-5.0.4-all-languages /var/www/phpmyadmin
-mv -f /tmp/config.inc.php /var/www/phpmyadmin/
 
 mv /tmp/localhost.conf /etc/nginx/conf.d/
 rm /etc/nginx/conf.d/default.conf

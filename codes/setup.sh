1cb3584e3ea19386cfc8629ee8aba096f616d97f
paikwiki <paikwiki@gmail.com>
2021-02-17 16:23:07 +0900

    WIP(fix error)

--- codes/setup.sh
+++ codes/setup.sh
@@ -21,11 +21,13 @@ docker build -t service-wordpress ./srcs/wordpress/
 docker build -t service-mysql ./srcs/mysql/
 docker build -t service-phpmyadmin ./srcs/phpmyadmin/
 docker build -t service-nginx ./srcs/nginx/
+docker build -t service-grafana ./srcs/grafana/
 
 kubectl apply -f ./srcs/mysql/mysql.yaml
 kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
 kubectl apply -f ./srcs/wordpress/wordpress.yaml
 kubectl apply -f ./srcs/nginx/nginx.yaml
+kubectl apply -f ./srcs/grafana/grafana.yaml
 
 # kubectl delete -f ./srcs/phpmyadmin/phpmyadmin.yaml;kubectl delete -f ./srcs/mysql/mysql.yaml
 # kubectl port-forward phpmyadmin-7cf4c5c785-mz25w 5000:80 -n default

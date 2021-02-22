acbcdbd6ee6092805886f3794f1d264a91a95460
paikwiki <paikwiki@gmail.com>
2021-02-23 00:45:18 +0900

    Refactor

--- codes/delete.sh
+++ codes/delete.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+## delete metallb
+kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+kubectl delete -f ./srcs/metallb_config.yaml
+
+## delete all components
+kubectl delete --all deployment
+kubectl delete --all services
+kubectl delete --all secret
+kubectl delete --all configmap
+kubectl delete --all pv,pvc
+
+rm -f ./srcs/nginx/nginx-index-config.yaml
+
+##delete all docker images && containers (in case)
+docker rmi $(docker images -q)
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
+
 minikube stop;
 minikube delete;
-rm -f ./srcs/nginx/nginx-index-config.yaml
+
+
--- codes/setup.sh
+++ codes/setup.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 
 # Minkube
+echo "==== Start Minikube ==========================================="
 minikube start --driver=virtualbox \
 			--extra-config=kubelet.authentication-token-webhook=true
-
-# Set Minikue addons
 minikube addons enable metallb
+echo "==== Done =====================================================\n"
+
+# Set env to Docker
+eval $(minikube docker-env)
+
+# Build Dokcer images
+echo "==== Build Docker images ======================================"
+docker build -t service-wordpress ./srcs/wordpress/
+docker build -t service-mysql ./srcs/mysql/
+docker build -t service-phpmyadmin ./srcs/phpmyadmin/
+docker build -t service-nginx ./srcs/nginx/
+docker build -t service-influxdb ./srcs/influxdb/
+docker build -t service-grafana ./srcs/grafana/
+docker build -t service-ftps ./srcs/ftps/
+echo "==== Done =====================================================\n"
 
 # MetalLB
+echo "==== Create Pods =============================================="
 kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl diff -f - -n kube-system
@@ -18,26 +33,15 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manife
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f ./srcs/metallb_config.yaml
 
-# Set env to Docker
-eval $(minikube docker-env)
-
-# Build Dokcer images
-docker build -t service-wordpress ./srcs/wordpress/
-docker build -t service-mysql ./srcs/mysql/
-docker build -t service-phpmyadmin ./srcs/phpmyadmin/
-docker build -t service-nginx ./srcs/nginx/
-docker build -t service-influxdb ./srcs/influxdb/
-docker build -t service-grafana ./srcs/grafana/
-docker build -t service-ftps ./srcs/ftps/
-
-# Apply all yaml files
+# Apply MySQL, Phpmyadmin, InfluxDB, Grafana Wordpress and Ftps
 kubectl apply -f ./srcs/mysql/mysql.yaml
 kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
 kubectl apply -f ./srcs/influxdb/influxdb.yaml
 kubectl apply -f ./srcs/grafana/grafana.yaml
-kubectl apply -f ./srcs/ftps/ftps.yaml
 kubectl apply -f ./srcs/wordpress/wordpress.yaml
-kubectl apply -f ./srcs/wordpress/wordpress-config.yaml
+kubectl apply -f ./srcs/ftps/ftps.yaml
+
+# Apply NginX server and create index page from template
 SITEIP="$(kubectl get services|grep wordpress|awk '{print $4}')";
 cat ./srcs/nginx/nginx-index-config-template.yaml | \
 sed -e "s/FT_SITEIP/$(echo -n $SITEIP)/" > ./srcs/nginx/nginx-index-config.yaml
@@ -45,3 +49,5 @@ diff ./srcs/nginx/nginx-index-config-template.yaml \
 		./srcs/nginx/nginx-index-config.yaml
 kubectl apply -f ./srcs/nginx/nginx-index-config.yaml
 kubectl apply -f ./srcs/nginx/nginx.yaml
+echo "==== Done =====================================================\n"
+echo "Enjoy FT_SERVICES!"
